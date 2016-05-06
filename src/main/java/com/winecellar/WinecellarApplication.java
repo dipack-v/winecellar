c1e742e7eac87d1ad15c207d6d44e5d223f9dabe
Deepak <dipack.v@gmail.com>
2016-05-06 18:12:24 +0530

    logging and exception

--- src/main/java/com/winecellar/WinecellarApplication.java
+++ src/main/java/com/winecellar/WinecellarApplication.java
@@ -1,13 +1,32 @@
 package com.winecellar;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.CustomizableTraceInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.winecellar.repository.WineRepository;
@@ -21,6 +40,16 @@ public class WinecellarApplication {
 	}
 	
 	@Bean
+	public CustomizableTraceInterceptor interceptor() {
+
+		CustomizableTraceInterceptor interceptor = new CustomizableTraceInterceptor();
+		interceptor.setEnterMessage("Entering $[methodName]($[arguments]).");
+		interceptor.setExitMessage("Leaving $[methodName](..) with return value $[returnValue], took $[invocationTime]ms.");
+
+		return interceptor;
+	}
+	
+	@Bean
 	CommandLineRunner init(WineRepository wineRepository) {
 		return args -> {
 			byte[] jsonData = IOUtils.toByteArray(new ClassPathResource("store.json").getInputStream());
@@ -31,3 +60,26 @@ public class WinecellarApplication {
 		};
 	}
 }
+
+
+@Aspect
+@Component
+class RepositoryMonitor {
+	@Before("execution( * org.springframework.data.repository.Repository+.*(..))")
+	public void logServiceAccess(JoinPoint joinPoint) {
+		System.out.println("Repository Method: " + joinPoint.getSignature().getName());
+		System.out.println("Arguments: " + Arrays.toString(joinPoint.getArgs()));
+	}
+
+}
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {  IllegalArgumentException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific !" ;
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
+
+
