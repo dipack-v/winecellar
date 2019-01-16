e8efdb4a718915e69a1fdc7b8505937705febe70
Deepak Vijayan Pillai <DeepakP@namindia.com>
2019-01-16 17:16:26 +0530

    become config client

--- src/main/java/com/winecellar/controller/WineController.java
+++ src/main/java/com/winecellar/controller/WineController.java
@@ -3,6 +3,7 @@ package com.winecellar.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,36 +15,42 @@ import com.winecellar.repository.entity.WineEntity;
 
 @RestController
 public class WineController {
+
+	@Value("${text.copyright: Default Copyright}")
+	private String copyright;
+
 	@Autowired
 	private WineRepository wineRepository;
-	
-	@RequestMapping(value="/wines", method = RequestMethod.GET)
-    public List<WineEntity> getWines() throws Exception {
-		 return wineRepository.findAll();
-    }
-	
-	@RequestMapping(value="/wines/{id}", method = RequestMethod.GET)
-    public WineEntity getWine(@PathVariable(value="id") Long id) {
-		return wineRepository.findById(id).get() ;
-    }
-	
-	@RequestMapping(value="/wines", method = RequestMethod.POST)
-    public WineEntity saveWine(@RequestBody WineEntity wine) throws Exception {
-		 return wineRepository.save(wine);
-    }
-	
-	@RequestMapping(value="/wines/{id}", method = RequestMethod.PUT)
-    public WineEntity updateWine(@PathVariable(value="id") Long id, @RequestBody WineEntity wine) throws Exception {
+
+	@RequestMapping(value = "/wines", method = RequestMethod.GET)
+	public List<WineEntity> getWines() throws Exception {
+		return wineRepository.findAll();
+	}
+
+	@RequestMapping(value = "/wines/{id}", method = RequestMethod.GET)
+	public WineEntity getWine(@PathVariable(value = "id") Long id) {
+		return wineRepository.findById(id).get();
+	}
+
+	@RequestMapping(value = "/wines", method = RequestMethod.POST)
+	public WineEntity saveWine(@RequestBody WineEntity wine) throws Exception {
 		return wineRepository.save(wine);
-    }
-	
-	@RequestMapping(value="/wines/{id}", method = RequestMethod.DELETE)
-    public void deleteWine(@PathVariable(value="id") Long id) {
+	}
+
+	@RequestMapping(value = "/wines/{id}", method = RequestMethod.PUT)
+	public WineEntity updateWine(@PathVariable(value = "id") Long id, @RequestBody WineEntity wine) throws Exception {
+		return wineRepository.save(wine);
+	}
+
+	@RequestMapping(value = "/wines/{id}", method = RequestMethod.DELETE)
+	public void deleteWine(@PathVariable(value = "id") Long id) {
 		wineRepository.deleteById(id);
-    }
-	
+	}
+
+	@RequestMapping("/show-config")
+	public String showConfig() {
+		String configInfo = "Copyright: " + copyright;
+		return configInfo;
+	}
+
 }
-
-
-
-
